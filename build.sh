b51e4f3c6a7221069eb3422e7a83900a27cd18ce
Frank Wiebeler <frank.wiebeler@dev4Agriculture.de>
2022-08-28 21:02:09 +0200

    Adjust DockerFile and Build Script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 docker stop isoxml-visualisation
 docker rm isoxml-visualization --force
 DOCKER_BUILDKIT=1 docker build --no-cache --ssh github=/home/frank/.ssh/rsa_id_github -t isoxml-visualization .
-docker run -p 8000:3000 --name isoxml-visualization --network net -e VIRTUAL_HOST="isoxml-viewer.dev4ag.com" -e VIRTUAL_PORT=8000 -e LETSENCRYPT_HOST="isoxml-viewer.dev4ag.com" -d isoxml-visualization & 
+docker run -p 8080:3000 --name isoxml-visualization --network net -e VIRTUAL_HOST="isoxml-viewer.dev4ag.com" -e VIRTUAL_PORT=8080 -e LETSENCRYPT_HOST="isoxml-viewer.dev4ag.com" -d isoxml-visualization & 
 
